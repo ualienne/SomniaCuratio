576c0d3719aebec2d5503fd71adfb36cbf269cdc
Ulyana <ulyanadyadkina@gmail.com>
2026-05-27 19:43:27 +0300

    Update assets/maps

---
+++ assets/maps/bureau_tilesets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="bureau" tilewidth="16" tileheight="16" tilecount="4128" columns="86">
+ <image source="bureau.png" width="1376" height="768"/>
+</tileset>
